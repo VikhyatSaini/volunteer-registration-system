b7bf1de3d6edc34c871cb05018d32b9326b657b2
VikhyatSaini <sainivikhyat045@gmail.com>
2025-12-15 21:01:01 +0530

    Fix nested git repository issue

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const generateToken = (id, role) => {
   return jwt.sign({ id, role }, process.env.JWT_SECRET, {
-    expiresIn: '30d', // Token will be valid for 30 days
+    expiresIn: '1d', // Token will be valid for 30 days
   });
 };
 
